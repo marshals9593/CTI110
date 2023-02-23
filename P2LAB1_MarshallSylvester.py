62e818076bddbd97319a64a6124a79d1a42c8229
marshals9593 <124936503+marshals9593@users.noreply.github.com>
2023-02-22 19:14:00 -0500

    Add files via upload
    P2LAB1

---
+++ P2LAB1_MarshallSylvester.py
@@ -0,0 +1,14 @@
+mileage = float(input())
+gas_cost = float(input())
+
+
+twenty_cost = 20 / mileage * gas_cost
+
+
+seventyfive_cost = 75 / mileage * gas_cost
+
+
+fivehundred_cost = 500 / mileage * gas_cost
+
+
+print(f'{twenty_cost:.2f} {seventyfive_cost:.2f} {fivehundred_cost:.2f}')
